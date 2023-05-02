8c5e0e4cc70f7a37f2ce9a55d90b2f05b16fec31
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-05-03 03:24:02 +0500

    Add webpack tools

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import './style.css';
 
-const myTodo = [
-  { description: 'wake up early', completed: true, index: 1 },
-  { description: 'setup for morning session', completed: false, index: 2 },
-  { description: 'Coding with microverse', completed: true, index: 3 },
-  { description: 'prepare for standup session', completed: false, index: 4 },
+const todoList = [
+  {
+    description: 'wake up early',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'setup for morning session',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Coding with microverse',
+    completed: true,
+    index: 2,
+  },
 ];
-const list = document.getElementById('list');
-// Sorting the array first
-myTodo.sort((a, b) => a.index - b.index);
 
-myTodo.forEach((value) => {
-  // Creating list of to-do
-  const li = document.createElement('li');
-  li.innerHTML = `
-        ${value.description}<i class="fa-solid fa-ellipsis-vertical"></i>
-    `;
-  list.appendChild(li);
-});
+const displayTodos = (todo) => {
+  const ul = document.querySelector('ul');
+  let content = '';
+  todo.forEach((tod) => {
+    content += `<li class="item">
+        <input type="checkbox" />
+        <p contenteditable="true">${tod.description}</p>
+      </li>`;
+    ul.innerHTML = content;
+  });
+
+  content = '';
+};
+displayTodos(todoList);
