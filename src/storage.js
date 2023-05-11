a786934cd6f179ee1c63418ead981b03564e3cfb
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-05-11 13:23:07 +0500

    Test add and delete item feature

--- src/storage.js
+++ src/storage.js
@@ -92,25 +92,6 @@ class Store {
         Store.check();
       });
     }
-    return {
-    setItem(key, value) {
-      storage[key] = value || '';
-    },
-    getItem(key) {
-      return key in storage ? storage[key] : null;
-    },
-    removeItem(key) {
-      delete storage[key];
-    },
-    get length() {
-      return Object.keys(storage).length;
-    },
-    key(i) {
-      const keys = Object.keys(storage);
-      return keys[i] || null;
-    },
-  };
-}
   };
 
   static createTask = (task) => {
